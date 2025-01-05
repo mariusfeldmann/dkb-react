b876a9bb64b2eb8a0e7d49bb7a155e8e562ae9b6
mariusfeldmann <feldmann.marius@gmail.com>
2025-01-05 20:58:22 +0000

    Deploying to gh-pages from @ mariusfeldmann/dkb-react@40e9808de72f6807956a65d34ba73bb57de0b48f 🚀

---
+++ substitutes.js
@@ -0,0 +1,4 @@
+window.subs = {
+  "Trade Republic Simon Carmiggeltstraat 1011 DJ AMSTERDAM 6-50": "Trade Republic",
+  "PayPal Europe S.a.r.l. et Cie S.C.A 22-24 Boulevard Royal, 2449 Luxembourg": "PayPal"
+}
